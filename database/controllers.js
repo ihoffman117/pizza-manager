e57edf3ffb999c70a2a7e183b8bec9dc1aa69fee
Ian Hoffman <ianhoffman@Ians-MacBook-Pro.local>
2022-10-03 13:59:32 -0700

    adding server routs and db controllers

--- database/controllers.js
+++ database/controllers.js
@@ -1,2 +1,11 @@
 const { Pizza, Topping } = require('./index');
 
+exports.getAllToppings = (req, res) => {
+  console.log('request for all toppings')
+  res.send('nothing to see here')
+}
+
+exports.getAllPizzas = (req, res) => {
+  console.log('request for all pizzas')
+  res.send('nothing to see here')
+}
--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const { getAllPizzas, getAllToppings } = require('../database/controllers')
 
 const app = express();
 
@@ -7,6 +8,14 @@ const port = 3000;
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '../client/dist')));
+
+app.get('/api/toppings', (req, res) => {
+  getAllToppings(req, res);
+})
+
+app.get('/api/pizzas', (req, res) => {
+  getAllPizzas(req, res);
+})
 
 app.listen(port, () => {
   console.log(`app listening on port ${port}`);
